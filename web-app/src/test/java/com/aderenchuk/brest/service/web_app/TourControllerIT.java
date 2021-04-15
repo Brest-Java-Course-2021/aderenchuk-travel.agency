876b14c808c11af336a3b0251d10410bb2179ff2
Artsiom Dzeranchuk <derenchuk2001@mail.ru>
2021-04-15 18:13:11 +0300

    first test in TourControllerIT.java

--- web-app/src/test/java/com/aderenchuk/brest/service/web_app/TourControllerIT.java
+++ web-app/src/test/java/com/aderenchuk/brest/service/web_app/TourControllerIT.java
@@ -1,22 +1,31 @@
 package com.aderenchuk.brest.service.web_app;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.math.BigDecimal;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @ExtendWith(SpringExtension.class)
 @WebAppConfiguration
 @ContextConfiguration(locations = {"classpath:app-context-test.xml"})
 @Transactional
-public class TourControllerIT {
+class DepartmentControllerIT {
 
     @Autowired
     private WebApplicationContext wac;
@@ -28,5 +37,35 @@ public class TourControllerIT {
         mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
     }
 
-
+    @Test
+    public void shouldReturnDepartmentsPage() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/tours")
+        ).andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/html;charset=UTF-8"))
+                .andExpect(view().name("tours"))
+                .andExpect(model().attribute("tours", hasItem(
+                        allOf(
+                                hasProperty("tourId", is(101)),
+                                hasProperty("direction", is("BREST-MINSK")),
+                                hasProperty("dateTour", is("2020-05-12"))
+                        )
+                )))
+                .andExpect(model().attribute("tours", hasItem(
+                        allOf(
+                                hasProperty("tourId", is(102)),
+                                hasProperty("direction", is("MINSK-DUBAI")),
+                                hasProperty("dateTour", is("2020-06-13"))
+                        )
+                )))
+                .andExpect(model().attribute("tours", hasItem(
+                        allOf(
+                                hasProperty("tourId", is(103)),
+                                hasProperty("direction", is("MOSCOW-BERLIN")),
+                                hasProperty("dateTour", is("2020-05-18"))
+                        )
+                )))
+        ;
+    }
 }
