bb6a492aba9426979898bb2fa7471df721221deb
Artsiom Dzeranchuk <derenchuk2001@mail.ru>
2021-04-18 20:43:59 +0300

    fix rest

--- rest-app/src/main/java/com/aderenchuk/brest/service/rest_app/exception/CustomExceptionHandler.java
+++ rest-app/src/main/java/com/aderenchuk/brest/service/rest_app/exception/CustomExceptionHandler.java
@@ -2,15 +2,13 @@ package com.aderenchuk.brest.service.rest_app.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(TourNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleDepartmentNotFound(TourNotFoundException e) {
+    public ResponseEntity<ErrorResponse> handleTourNotFound(TourNotFoundException e) {
         ErrorResponse error = new ErrorResponse(e.getMessage());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
