38326861b08986abb01e90bf36e67df1571f227b
artem-derenchuk <derenchuk2001@mail.ru>
2021-11-03 19:35:47 +0300

    TourController + schema

---
+++ soap-app/src/main/java/com/aderenchuk/brest/service/soap_app/ApplicationSoap.java
@@ -0,0 +1,13 @@
+package com.aderenchuk.brest.service.soap_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationSoap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationSoap.class, args);
+    }
+
+}
